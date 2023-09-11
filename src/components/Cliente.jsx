08034dd76928f1d7aa9a7dac68bd97130c5f5d71
JuanRaV <juanravirgen@gmail.com>
2023-09-11 16:18:59 -0600

    Starting edit function

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom"
 
 const Cliente = ({cliente}) => {
+    const navigate = useNavigate();
     const {nombre,empresa,email,telefono,id} = cliente
+
   return (
     <>
         <tr className="border-b">
@@ -21,6 +24,7 @@ const Cliente = ({cliente}) => {
                 <button
                     type="button"
                     className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                    onClick={()=>navigate(`/clientes/${id}/editar`)}
                 >
                     Editar
                 </button>
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Layout from './components/Layout'
 import NuevoCliente,{action as nuevoClienteAction} from './pages/NuevoCliente'
 import Index, {loader as clientesLoader} from './pages/Index'
 import ErrorPage from './components/ErrorPage'
+import EditarCliente , {loader as editarClienteLoader}from './pages/EditarCliente'
 
 //Routing con diferentes URLS
 const router = createBrowserRouter([  
@@ -24,6 +25,11 @@ const router = createBrowserRouter([
         path:'/clientes/nuevo',
         element: <NuevoCliente/>,
         action: nuevoClienteAction
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente/>,
+        loader: editarClienteLoader
       }
     ]
   }
