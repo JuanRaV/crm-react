ea6582cf804d1d26cad902ad2d7ccb9dbe0ef3c3
JuanRaV <juanravirgen@gmail.com>
2023-09-11 14:28:38 -0600

    Adding validation to form

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,9 +1,28 @@
-import {useNavigate} from 'react-router-dom'
+import {useNavigate,Form,useActionData} from 'react-router-dom'
 import Formulario from '../components/Formulario'
+import Error from '../components/Error'
+
+export async function action({request}){
+  const formData = await request.formData()
+  //Obtenemos la informacion ingresada al formulario en forma de objeto
+  const datos = Object.fromEntries(formData)
+  
+  //Validacion
+  const errores = []
+  if(Object.values(datos).includes('')){
+    errores.push('Todos los campos son obligatorios')
+  }
+  //Retornar datos y errores
+  if(Object.keys(errores).length){
+    return errores
+  }
+
+}
 
 const NuevoCliente = () => {
-
+  const errores = useActionData()
   const navigate = useNavigate()
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -19,14 +38,17 @@ const NuevoCliente = () => {
       </div>
 
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
-        <form action="">
+        {errores?.length && errores.map((error,i)=><Error key={i}>{error}</Error>)}
+        <Form 
+          method='POST'
+        >
           <Formulario/>
           <input
             type='submit'
             className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg hover:bg-blue-900 '
             value="Registrar Cliente"
           />
-        </form>
+        </Form>
       </div>
     </>
   )
