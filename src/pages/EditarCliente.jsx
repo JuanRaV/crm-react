222fdce7df4438274d18a351c922cd686b963066
JuanRaV <juanravirgen@gmail.com>
2023-09-12 10:01:05 -0600

    Verifying if a client exists and showing error page

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,8 +1,14 @@
 import {obtenerCliente} from '../data/clientes'
 export async function loader({params}){
   const cliente = await obtenerCliente(params.clienteId)
-  console.log(cliente)
-  return null
+  if(Object.values(cliente).length===0){
+    //Creamos nuestra propia respuesta
+    throw new Response('',{
+      status:404,
+      statusText: 'No hay resultados'
+    })
+  }
+  return cliente
 }
 
 const EditarCliente = () => {
